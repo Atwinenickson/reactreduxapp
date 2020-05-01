ff1dfe7b7c00eb114d6700aeaa9cb348278e71b9
Atwinenickson <atwiinenicxon@gmail.com>
2020-05-01 13:57:50 +0300

    create new react app, add actions, reducers, componets

---
+++ reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
+import postReducer from './postReducer'
+import comments from './commentsReducer'
+const rootReducer = combineReducers({
+    posts: postsReducer,
+    post: postReducer,
+    comments:commentsReducer
+});
+
+export default rootReducer;
